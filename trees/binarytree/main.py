3824cec6167f812755e58b9db92f0948cbf87e8d
Andrés Martínez <autummata@gmail.com>
2026-01-05 00:40:50 -0700

    refactor: build tree method

--- trees/binarytree/main.py
+++ trees/binarytree/main.py
@@ -1,28 +1,34 @@
-# Binary tree implementation 
+# Binary tree implementation
 class TreeNode: 
     def __init__(self, val):
         self.val = val
         self.left = None 
         self.right = None 
 
-root = TreeNode('R')
-nodeA = TreeNode('A')
-nodeB = TreeNode('B')
-nodeC = TreeNode('C')
-nodeD = TreeNode('D')
-nodeE = TreeNode('E')
-nodeF = TreeNode('F')
-nodeG = TreeNode('G')
 
-root.left = nodeA
-root.right = nodeB
+# This method builds an example tree based on previous impl.
+# Should be ready for other modules to use
+def build_tree_example() -> list[TreeNode]:
+    root = TreeNode("R")
+    nodeA = TreeNode("A")
+    nodeB = TreeNode("B")
+    nodeC = TreeNode("C")
+    nodeD = TreeNode("D")
+    nodeE = TreeNode("E")
+    nodeF = TreeNode("F")
+    nodeG = TreeNode("G")
 
-nodeA.left = nodeC
-nodeA.right = nodeD
+    root.left = nodeA
+    root.right = nodeB
 
-nodeB.left = nodeE
-nodeB.right = nodeF
+    nodeA.left = nodeC
+    nodeA.right = nodeD
 
-nodeF.left = nodeG
+    nodeB.left = nodeE
+    nodeB.right = nodeF
+
+    nodeF.left = nodeG
+    return root
 
 
+tree = build_tree_example()
